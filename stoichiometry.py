84d7b07edb2496a2f93673a1452adffcf5c771b1
Jhanny-Kin <88437453+Jhanny-Kin@users.noreply.github.com>
2023-02-24 10:38:44 +0800

    Update stoichiometry.py

--- stoichiometry.py
+++ stoichiometry.py
@@ -1,7 +1,12 @@
-# PROGRAM IS UNDER RECONSTRUCTION
+from copy import deepcopy
+from time import time
+
+
 diatomic = ['O', 'N', 'H', 'F', 'Cl', 'I', 'Br']
+activity = ['Li', 'K', 'Ba', 'Sr', 'Ca', 'Na', 'Mg', 'Al', 'Zn', 'Cr', 'Fe', 'Cd', 'Co', 'Ni', 'Sn', 'Pb', 'H', 'Cu', 'Hg', 'Ag', 'Pt', 'Au', 'F', 'Cl', 'Br', 'I']
 path = 'data.csv'
 table = {}
+
 
 with open(path) as file:
     for each in file.readlines():
@@ -43,7 +48,7 @@ def expand(molecule=str):
             if molecule[end+1:i] != '':
                 quantity = int(molecule[end+1:i])
         else:
-            return None
+            return False
 
         molecule = molecule[:begin] + molecule[i:]
         result.insert(0, [quantity, information])
@@ -72,6 +77,7 @@ class Molecule():
         self.atoms = []
         self.coef = 1
         self.bond = None
+        self.mass = 0
 
         if name is not None:
             expansion = expand(name)
@@ -92,8 +98,22 @@ class Molecule():
         if flag >= 2:
             self.bond = 'organic'
 
+
+    def weigh(self):
+        self.mass = 0
+        for atom in self.atoms:
+            self.mass += (atom.mass * atom.quantity)
+        self.mass *= self.coef
+
+
+    def show(self):
+        print(f'Coefficient: {self.coef} | Bond: {self.bond} | Mass: {self.mass}')
+        for atom in self.atoms:
+            print(f'{atom.__dict__}')
+
+
     def rectify(self):
-        if self.bond == 'ionic':
+        if 'ionic' in self.bond:
             if self.atoms[0].charge[0] > 0:
                 self.atoms[0], self.atoms[1] = self.atoms[1], self.atoms[0]
             charge = abs(self.atoms[0].quantity * self.atoms[0].charge[0])
@@ -102,25 +122,138 @@ class Molecule():
                     self.atoms[1].charge = [self.atoms[1].charge[i]]
                     self.bond = '*ionic'
                     break
+        elif self.bond == 'single' and self.atoms[0].name in diatomic:
+            self.atoms[0].quantity = 2
+        self.weigh()
 
     def form(self):
-        if self.bond == 'ionic':
-            lcm = abs(self.atoms[0].quantity * self.atoms[0].charge[0])
+        if 'ionic' in self.bond:
+            lcm = 1
             while lcm % self.atoms[0].charge[0] or lcm % self.atoms[1].charge[0]:
                 lcm += 1
             self.atoms[0].quantity = int(abs(lcm / self.atoms[0].charge[0]))
             self.atoms[1].quantity = int(abs(lcm / self.atoms[1].charge[0]))
             self.bond = '*ionic'
+        self.weigh()
 
 
 class Equation():
-    def __init__(self, *args) -> None:
+    def __init__(self, equation) -> None:
+        self.short = tuple(equation)
         self.comp = []
-        self.type = None
+        self.reaction = None
+        self.counter = {}
 
-        for each in args:
+        for each in equation:
             molecule = Molecule(each)
+            molecule.rectify()
             self.comp.append(molecule)
 
+    def show(self):
+        print('-' * 100)
+        print(f'{self.short} | {self.reaction}')
+        for molecule in self.comp:
+            molecule.show()
 
-reactant = Equation('Na(OH)', 'HCl')
+    def react(self):
+        if len(self.comp) == 1 and len(self.comp[0].atoms) == 2:
+            self.reaction = 'decompose'
+        elif len(self.comp) == 2:
+            if self.comp[0].bond == 'single' and self.comp[1].bond == 'single':
+                charge = self.comp[0].atoms[0].charge[0] * self.comp[1].atoms[0].charge[0]
+                self.reaction = '*combine' if charge < 0 else 'combine'
+            for i in range(2):
+                if self.comp[0-i].bond == 'organic' and self.comp[1-i].atoms[0].name == 'O':
+                    self.reaction = 'combust'
+                elif self.comp[0-i].bond == 'single' and self.comp[1-i].bond == '*ionic':
+                    self.reaction = 'single'
+                elif self.comp[0-i].bond == '*ionic' and self.comp[1-i].bond == '*ionic':
+                    if 'H' in self.short[0-i] and '(OH)' in self.short[1-i]:
+                        self.reaction = 'neutralize'
+                        break
+                    else:
+                        self.reaction = 'double'
+        else:
+            return False
+                    
+    
+    def predict(self):
+        def decompose():
+            a = f'{self.comp[0].atoms[0].name}{self.comp[0].atoms[0].quantity}'
+            b = f'{self.comp[0].atoms[1].name}{self.comp[0].atoms[1].quantity}'
+            product = Equation([a, b])
+            return product
+
+
+        def combine():
+            product = Equation([f'({self.comp[0].atoms[0].name})({self.comp[1].atoms[0].name})'])
+            return product
+        
+        
+        def combust():
+            for molecule in self.short:
+                product = Equation(['H2O', 'CO2', 'SO2']) if 'S' in molecule else Equation(['H2O', 'CO2'])
+            return product
+                
+
+        def single():
+            if len(self.comp[0].atoms) > len(self.comp[1].atoms):
+                self.comp[0], self.comp[1] = self.comp[1], self.comp[0]
+            product = deepcopy(self)
+            if activity.index(self.comp[0].atoms[0].name) < activity.index(self.comp[1].atoms[1].name):
+                product.comp[0].atoms[0], product.comp[1].atoms[1] = product.comp[1].atoms[1], product.comp[0].atoms[0]
+            return product
+        
+
+        def neutralize():
+            if self.comp[0].atoms[0].name == 'OH':
+                salt = f'({self.comp[0].atoms[1].name})({self.comp[1].atoms[0].name})'
+            else:
+                salt = f'({self.comp[0].atoms[0].name})({self.comp[1].atoms[1].name})'
+            product = Equation([salt, 'H2O'])
+            return product
+            
+
+        def double():
+            product = deepcopy(self)
+            product.comp[0].atoms[0], product.comp[1].atoms[0] = product.comp[1].atoms[0], product.comp[0].atoms[0]
+            return product
+
+
+        if self.react() is False:
+            product = None
+        elif self.reaction == 'decompose':
+            product = decompose()
+        elif self.reaction == '*combine':
+            product = combine()
+        elif self.reaction == 'combust':
+            product = combust()
+        elif self.reaction == 'single':
+            product = single()
+        elif self.reaction == 'neutralize':
+            product = neutralize()
+        elif self.reaction == 'double':
+            product = double()
+    
+        if product is not None:
+            for molecule in product.comp:
+                molecule.form()
+
+            update = []
+            for molecule in product.comp:
+                name = str(molecule.coef)
+                for atom in molecule.atoms:
+                    name += f'({atom.name}){atom.quantity}'
+                update.append(name)
+            product.short = tuple(update)
+
+        return product
+
+
+def balance(*args, max=20):
+    reactant = Equation([each for each in args])
+    product = reactant.predict()
+
+
+if __name__ == '__main__':
+    balance('Na(OH)', 'HCl')
